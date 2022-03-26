82abf33494627341324e3e2e5c86ed356d989d1a
Carolaine Vieira <carolaine.vieira@apiki.com>
2022-03-25 23:03:54 -0300

    JobCard styled state

--- src/pages/JobList/index.jsx
+++ src/pages/JobList/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Header from '../../components/Header'
 
 function JobList() {
-  return <div>JobList</div>
+  return(
+    <>
+      <Header />
+    </>
+  )
 }
 
 export default JobList
