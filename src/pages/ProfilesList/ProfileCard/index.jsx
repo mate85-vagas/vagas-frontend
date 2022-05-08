09f972ebdff86ef3d57418294a46c1c7b7980014
Carolaine Vieira <cavithemes@gmail.com>
2022-05-08 12:47:09 -0300

    feat: profile card finished

--- src/pages/ProfilesList/ProfileCard/index.jsx
+++ src/pages/ProfilesList/ProfileCard/index.jsx
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types'
 import ButtonRectangle from '../../../components/Buttons/ButtonRectangle'
 import './style.css'
 
-function ProfileCard({ id, name, resume }) {
+function ProfileCard({ id, name, resume, knowledge }) {
   return (
     <div className="profile-card" id={`profile-${id}`}>
       <div className="left-container">
         <h3>{name}</h3>
-        <span>Outra info</span>
+        <span>{knowledge}</span>
       </div>
       <div className="right-container">
         <ButtonRectangle
           label="Perfil"
           className="is-blue"
           onClick={() => {
-            window.open(`${resume}`)
+            if (resume) window.open(`${resume}`)
           }}
         />
       </div>
@@ -27,12 +27,14 @@ ProfileCard.propTypes = {
   id: PropTypes.number,
   name: PropTypes.string,
   resume: PropTypes.string,
+  knowledge: PropTypes.string,
 }
 
 ProfileCard.defaultProps = {
   id: 0,
   name: 'User Name',
   resume: '/',
+  knowledge: '',
 }
 
 export default ProfileCard
